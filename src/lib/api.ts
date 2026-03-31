7b14ef7286d232aac726e6435d11bd9337a0361f
Pedro Lucena <pedrolucena.dev@gmail.com>
2026-03-31 09:30:49 -0300

    feat: add api.ts

---
+++ src/lib/api.ts
@@ -0,0 +1,28 @@
+import { env } from "@/utils";
+
+export async function post<RequestBody, ResponseBody>(
+  path: string,
+  body: RequestBody
+  ): Promise<ResponseBody> {
+  const url = `${env.backend.apiUrl}${path}`;
+  
+  const res = await fetch(
+    url, 
+    {
+      method: "POST",
+      headers: buildHeaders(),
+      body: JSON.stringify(body),
+    }
+  );
+  
+  return res.json() as Promise<ResponseBody>;
+}
+
+function buildHeaders(): HeadersInit {
+  const credentials = btoa(`${env.backend.loginUsername}:${env.backend.loginPassword}`);
+
+  return {
+    Authorization: `Basic ${credentials}`,
+    "Content-Type": "application/json",
+  };
+}
---
+++ src/utils/env.ts
@@ -0,0 +1,7 @@
+export const env = {
+  backend: {
+    apiUrl: process.env.BACKEND_API_URL as string,
+    loginUsername: process.env.BACKEND_LOGIN_USERNAME as string,
+    loginPassword: process.env.BACKEND_LOGIN_PASSWORD as string,
+  }
+};
